e982e6ad4ee84eae365c92eb8e8371bc0850c850
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2023-01-21 19:39:18 +0530

    🔥 Backend Updated

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import loofeRoutes from "./routes/loofeRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/loofe", loofeRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Hello from LOOF-E AI!");
